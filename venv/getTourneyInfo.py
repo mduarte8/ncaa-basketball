0828347c49f12ada7abefd648ea8909e3ad538e8
Mark Duarte <duarte.mark8@gmail.com>
2023-03-13 08:30:20 -0700

    scraped page with table of champions decades past, got team name, tourney link, year and created json file

--- venv/getTourneyInfo.py
+++ venv/getTourneyInfo.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = 'https://www.sports-reference.com/cbb/postseason/'
 response = requests.get(url)
@@ -10,7 +11,33 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 body = soup.find('tbody')
 
+champions = []
+championLinks = []
+
 for row in body.find_all('tr'):
     print(row.prettify())
-    link = row.find('a')
-    print(link.get('href'))
+    header = row.find('th')
+    link = header.find('a')
+    if link:  # need if link exists as last row in table may not have NCAA tourney but NIT tourney which we don't want
+        # print("header tag is", header)
+        url = link['href']
+        # Takes the first column with link of year and NCAA and splits text string into array and grabs first element which should be year e.g. 1980 NCAA will return 1980
+        year = header.text.split()[0]
+        championTag = row.find('td', {"data-stat": "ncaa_champ"})
+        champion = championTag.text
+        yearObj = {}
+        # print("url is", url)
+        # print("url is type", type(url))
+        yearObj[year] = {"champion": champion, "url": url}
+        champions.append(yearObj)
+        # print("yearObj is", yearObj)
+        # print("accessing Obj,", yearObj[year])
+        # print("YEAR is", year)
+        # print("!!! champion is", champion)
+        # print("CHAMP LINK IS", link.get('href'))
+        # print("*"*30)
+        print("yearObj.champion is", yearObj[year]["champion"])
+# print(champions)
+
+with open('champions.json', 'w') as f:
+    json.dump(champions, f, indent=2)
